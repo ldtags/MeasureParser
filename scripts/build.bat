bb092314431817f769322afb6760bd92a9f953d5
Liam Tangney <ltags230@gmail.com>
2024-01-03 23:48:52 -0600

    updated bat files

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,8 +5,8 @@ rem This script automates the PyInstaller executable building process
 set SPEC_NAME=parser
 if not "%1"=="" set SPEC_NAME=%1
 
-set DB_FILE=measureparser\data\database.db
-set SCHEMA_FILE=measureparser\data\measure.schema.json
+set DB_FILE=measureparser/data/database.db
+set SCHEMA_FILE=measureparser/data/measure.schema.json
 
 cd %~dp0\..
 call pyinstaller --clean --noconsole -y -n "%SPEC_NAME%"^
--- scripts/clean.bat
+++ scripts/clean.bat
@@ -2,3 +2,6 @@
 
 rem This script is used for removing build files from the directory
 
+cd %~dp0\..
+rmdir /s /q "build", "dist"
+del /q /f *.spec
