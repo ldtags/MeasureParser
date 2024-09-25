d2e347de5cabdc6a4cb7cc3f49a066625e921730
Liam Tangney <ltags230@gmail.com>
2024-09-25 12:06:48 -0500

    updated get path method

--- tests/permqaqc/resources/__init__.py
+++ tests/permqaqc/resources/__init__.py
@@ -4,9 +4,9 @@ import os
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_path(*path_fragments: str) -> str:
+def get_path(*path_fragments: str, exists: bool=True) -> str:
     file_path = os.path.join(_ROOT, *path_fragments)
-    if not os.path.exists(file_path):
+    if not os.path.exists(file_path) and exists:
         raise FileExistsError(f'No file with the path {file_path} exists')
 
     return file_path
